fdfe5013f20e73b4ee56b47364c080fbb5cbdfcc
DmitryShadrin <dmitry.shadrin.alex1989@gmail.com>
2022-11-19 09:47:36 +0300

    G

--- src/yandex/sprint1/F.java
+++ src/yandex/sprint1/F.java
@@ -1,9 +1,9 @@
 package yandex.sprint1;
 
-        import java.io.BufferedReader;
-        import java.io.IOException;
-        import java.io.InputStreamReader;
-        import java.util.Locale;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Locale;
 
 // Палиндром
 public class F {
