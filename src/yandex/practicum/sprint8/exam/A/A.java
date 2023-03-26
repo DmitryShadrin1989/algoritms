7fbee42d34c2b990d7fa3e33581c6aea18d1b406
DmitryShadrin <DShadrin@sportmaster.ru>
2023-03-26 14:13:17 +0300

    study sprint 8

--- src/yandex/practicum/sprint8/exam/A/A.java
+++ src/yandex/practicum/sprint8/exam/A/A.java
@@ -39,165 +39,165 @@ abacabaca
 
 Вывод
 aba
+ */
 
+/*
+-- ПРИНЦИП РАБОТЫ --
+Программа использует два метода:
+1. Распаковка строки unpackString.
+Для реализации использовал Stack<String> (перепробовал массу вариантов и этот оказался самым быстрым).
+Обходим переданную строку посимвольно и все кладем в стек пока не встретим закрывающую скобку - ']'.
+Тогда мы начинаем доставать элементы из стека и конкатенировать строку пока не встретим открывающую скобку - '['.
+За ней должен стоять множитель n, по условию задачи это "однозначное натуральное число".
+Повторяем конкатенированную строку n раз и кладем обратно в стек, т.е. мы раскрыли одну скобку и положили получившееся значение обратно.
+Так повторяем пока не пройдем всю строку. Далее начинаем вынимать элементы из стека в обратном порядке.
+
+2. Поиск индекса которым заканчивается наибольший префикс для строк findMaxPrefIdx выполняется для пары сравниваемых строк.
+Применяется принцип бинарного поиска, т.е. мы делим все время строку пополам, чтобы найти максимальный индекс на котором строки равны.
+Для оптимизации добавил, чтобы для правой границы передавался уже вычисленный на предыдущей итерации максимальный индекс totalMaxPrefIdx.
+Он подставляется в качестве правой границы.
+
+-- ДОКАЗАТЕЛЬСТВО КОРРЕКТНОСТИ --
+Честно не успел заполнить этот пункт.
+
+-- ВРЕМЕННАЯ СЛОЖНОСТЬ --
+По распаковке. Процедура выполняется для каждой строки, проходит по всем элементам исходной (запакованной строки) строки и
+по всем элементам после ее распаковки. Сложность получается O(N * (M + K)), где N - количество строк, M - длинна запакованной строки,
+K - длинная распакованной строки.
+
+По поиску индекса наибольшего префикса. Процедура сравнивает пары распакованных строк т.е. N-1, где N - количество строк.
+Для каждой такой пары сравнение выполняется за O(logK), где K - длинная распакованной строки.
+Т.е. общая сложность поиска индекса O((N-1)logK).
+
+Общая временная сложность алгоритма O(N*(M + K) + (N-1)*logK) ~ O(N*K)
+
+-- ПРОСТРАНСТВЕННАЯ СЛОЖНОСТЬ --
+Т.к. массив String[] пере используется пространственная сложность будет O(N*K), где N - количество строк, K - длинная распакованной строки.
+
+Успешная посылка - https://contest.yandex.ru/contest/26133/run-report/84617614/
  */
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Stack;
 
 public class A {
     public static void main(String[] args) throws IOException {
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
 
-            System.out.println(unpackString3(reader.readLine()));
+            int n = Integer.parseInt(reader.readLine());
+            String[] strings = new String[n];
+            for (int i = 0; i < n; i++) {
+                strings[i] = reader.readLine();
+            }
 
+            if (strings.length == 1) {
+                System.out.println(unpackString(strings[0]));
+                return;
+            }
 
-//            int n = Integer.parseInt(reader.readLine());
-//            String[] strings = new String[n];
-//            for (int i = 0; i < n; i++) {
-//                strings[i] = reader.readLine();
-//            }
-//
-//            int totalMaxPrefIdx = -1;
-//            for (int i = 0; i < n; i++) {
-//                strings[i] = unpackString2(strings[i]);
-//
-//                if (i == 0) {
-//                    continue;
-//                }
-//                String currentString = strings[i];
-//                String prevString = strings[i-1];
-//                int j = 0;
-//                int currentMaxPrefIdx = 0;
-//                while (j < currentString.length() && j < prevString.length()) {
-//                    if (currentString.charAt(j) != prevString.charAt(j)) {
-//                        break;
-//                    }
-//                    currentMaxPrefIdx++;
-//                    j++;
-//                }
-//
-//                if (totalMaxPrefIdx == -1) {
-//                    totalMaxPrefIdx = currentMaxPrefIdx;
-//                }
-//                totalMaxPrefIdx = Math.min(totalMaxPrefIdx, currentMaxPrefIdx);
-//            }
-//
-//            System.out.println(strings[0].substring(0, totalMaxPrefIdx));
+            int totalMaxPrefIdx = -1;
+            for (int i = 0; i < n; i++) {
+                strings[i] = unpackString(strings[i]);
+
+                if (i == 0) {
+                    continue;
+                }
+                String currentString = strings[i];
+                String prevString = strings[i-1];
+
+                int currentMaxPrefIdx = findMaxPrefIdx(currentString, prevString, totalMaxPrefIdx);
+
+                if (totalMaxPrefIdx == -1) {
+                    totalMaxPrefIdx = currentMaxPrefIdx;
+                }
+
+                totalMaxPrefIdx = Math.min(totalMaxPrefIdx, currentMaxPrefIdx);
+            }
+            StringBuilder result = new StringBuilder();
+            for (int i = 0; i < totalMaxPrefIdx; i++) {
+                result.append(strings[0].charAt(i));
+            }
+            System.out.println(result);
         }
     }
 
-    static String unpackString(String string) {
-        byte[] bytes = string.getBytes();
-        Stack<Byte> stack = new Stack<>();
-        StringBuilder result = new StringBuilder();
-        for (int i = 0; i < bytes.length; i++) {
-            byte currentByte = bytes[i];
+    private static int findMaxPrefIdx(String currentString, String prevString, int totalMaxPrefIdx) {
+        int left = 0;
+        int right = totalMaxPrefIdx == -1 ? Math.min(currentString.length(), prevString.length()) : totalMaxPrefIdx;
+        int mid;
+        while (right - left > 1) {
+            mid = (left + right) / 2;
 
-            if ((char) currentByte == ']') {
-                StringBuilder currentString = new StringBuilder();
-                while (!stack.isEmpty()) {
-                    int countOpenBracket = 0;
-                    byte pop = stack.pop();
-                    if ((char) pop == '[') {
-                        countOpenBracket = countOpenBracket + 1;
-                        if (countOpenBracket>1) {
-                            break;
-                        }
-                        continue;
-                    }
-                    if (pop > 47 && pop < 58) {
-                        int currentNumber = Integer.parseInt(String.valueOf((char) pop));
-                        String s = currentString.toString();
-                        for (int j = 0; j < currentNumber-1; j++) {
-                            currentString.append(s);
-                        }
-                    } else {
-                        currentString.append((char) pop);
-                    }
-                }
-                result.append(currentString.reverse());
+            if (currentString.charAt(mid) == prevString.charAt(mid)) {
+                left = mid;
             } else {
-                stack.push(currentByte);
+                right = mid;
             }
         }
-        if (!stack.isEmpty()) {
-            for (int i = 0; i < stack.size(); i++) {
-                byte b = stack.get(i);
-                result.append((char) b);
+        return right;
+    }
+
+    static String unpackString(String string) {
+        String[] strings = string.split("");
+        Stack<String> stack = new Stack<>();
+        for (int i = 0; i < strings.length; i++) {
+            String currenString = strings[i];
+
+            if (currenString.equals("]")) {
+                StringBuilder local = new StringBuilder();
+
+                while (!stack.peek().equals("[")) {
+                    local.append(stack.pop());
+                }
+                stack.pop();
+                int factorInt = Integer.parseInt(stack.pop());
+                String s = local.toString();
+                for (int j = 0; j < factorInt-1; j++) {
+                    local.append(s);
+                }
+                stack.push(local.toString());
+            } else {
+                stack.push(currenString);
+            }
+        }
+        StringBuilder result = new StringBuilder();
+        for (String s : stack) {
+            for (int j = s.length() - 1; j >= 0; j--) {
+                result.append(s.charAt(j));
             }
         }
         return result.toString();
     }
 
     static String unpackString2(String string) {
-        byte[] bytes = string.getBytes();
-        Stack<Byte> stack = new Stack<>();
-        StringBuilder decodeString = new StringBuilder();
-        //3[a]2[r2[t]]
-        for (int i = 0; i < bytes.length; i++) {
-            byte currentByte = bytes[i];
+        Stack<Character> stack = new Stack<>();
 
-            if ((char) currentByte == ']') {
-                StringBuilder intermediateResult = new StringBuilder();
+        for (int i = 0; i < string.length(); i++) {
+            if (string.charAt(i) == ']') {
+                StringBuilder decodedString = new StringBuilder();
 
                 while (stack.peek() != '[') {
-                    byte pop = stack.pop();
-                    intermediateResult.append((char) pop);
+                    decodedString.append(stack.pop());
                 }
                 stack.pop();
-                byte factor = stack.pop();
-                int factorInt = Integer.parseInt(String.valueOf((char) factor));
-                String s = intermediateResult.reverse().toString();
-
+                int factorInt = Integer.parseInt(stack.pop().toString());
+                String s = decodedString.toString();
                 for (int j = 0; j < factorInt; j++) {
-                    decodeString.append(s);
-                }
-
-            } else {
-                stack.push(currentByte);
-            }
-        }
-        return decodeString.toString();
-    }
-
-    static String unpackString3(String string) {
-
-        byte[] bytes = string.getBytes();
-        Stack<Byte> stack = new Stack<>();
-        StringBuilder decodeString = new StringBuilder();
-        //3[a]2[r2[t]]
-        for (int i = 0; i < bytes.length; i++) {
-            byte currentByte = bytes[i];
-
-            if ((char) currentByte == ']') {
-                ArrayList<Byte> byteArrayList = new ArrayList<>();
-
-                while (stack.peek() != '[') {
-                    byteArrayList.add(stack.pop());
-                }
-                stack.pop();
-                byte factor = stack.pop();
-                int factorInt = Integer.parseInt(String.valueOf((char) factor));
-                for (int j = 0; j < factorInt; j++) {
-                    for (int k = byteArrayList.size()-1; k>=0; k--) {
-                        stack.push(byteArrayList.get(k));
+                    for (int k = s.length() - 1; k >= 0; k--) {
+                        stack.push(s.charAt(k));
                     }
                 }
             } else {
-                stack.push(currentByte);
+                stack.push(string.charAt(i));
             }
         }
+
         StringBuilder result = new StringBuilder();
-        while (!stack.isEmpty()) {
-            byte b = stack.pop();
-            result.append((char) b);
+        for (char ch : stack) {
+            result.append(ch);
         }
-
-        return result.reverse().toString();
-
+        return result.toString();
     }
 }
